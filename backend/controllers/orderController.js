ff2c2a9d8084c807afdd7795600cd4963c3a011b
hamzamughal <mughalh473@gmail.com>
2021-06-02 11:40:07 +0500

    order Screen added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler"
 import Product from '../models/productModel.js'
+import Order from '../models/orderModel.js'
 
 // @desc     Create new Order
 // @route    POST /api/orders
@@ -22,9 +23,22 @@ const addOrderItems = asyncHandler(async (req, res) => {
         res.status(201).json(createdOrder)
 
     }
+})
 
+// @desc     Get order by ID
+// @route    GET /api/orders/:id
+// @access   Private
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+    if (order) {
+        res.json(order)
+    } else {
+        res.status(404)
+        throw new Error('Order not found!')
+    }
 })
 
 export {
-    addOrderItems
+    addOrderItems, getOrderById
 }
